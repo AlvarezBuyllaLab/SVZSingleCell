8c676a4bc1320be2d9c746fabc32cc0a6b5afff5
Marcos Assis Nascimento <66184172+massisnascimento@users.noreply.github.com>
2021-03-15 15:26:02 -0700

    Update get.fastq.files.sh

--- Pre-processing/scRNA-Seq/fastq.files/get.fastq.files.sh
+++ Pre-processing/scRNA-Seq/fastq.files/get.fastq.files.sh
@@ -1,3 +1,3 @@
-module load sratoolkit
-fastq-dump --split-files --origfmt --gzip SRR13532408
-fastq-dump --split-files --origfmt --gzip SRR13532410
+#you need sratoolkit installed, more info on: https://github.com/ncbi/sra-tools/wiki/02.-Installing-SRA-Toolkit
+fasterq-dump --split-files --origfmt --gzip SRR13532408
+fasterq-dump --split-files --origfmt --gzip SRR13532410
