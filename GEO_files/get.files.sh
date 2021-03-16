f1408bb7efb903e490492bf40d0550906f1aa3f1
Marcos Assis Nascimento <66184172+massisnascimento@users.noreply.github.com>
2021-03-15 18:09:52 -0700

    Update get.files.sh

--- GEO_files/get.files.sh
+++ GEO_files/get.files.sh
@@ -1,10 +1,18 @@
 wget -O GSE165554_RAW.tar "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE165554&format=file"
 
+tar -xvf GSE165554_RAW.tar
+
 mkdir outs
 mkdir outs/g1
 mkdir outs/g2
 mkdir seurat
 
-tar -xvf GSE165554_RAW.tar GSM5039270_G1_CellRanger_outs_barcodes.tsv.gz GSM5039270_G1_CellRanger_outs_features.tsv.gz GSM5039270_G1_CellRanger_outs_matrix.mtx.gz -C /outs/g1
-tar -xvf GSE165554_RAW.tar GSM5039270_G2_CellRanger_outs_barcodes.tsv.gz GSM5039270_G2_CellRanger_outs_features.tsv.gz GSM5039270_G2_CellRanger_outs_matrix.mtx.gz -C /outs/g2
-tar -xvf GSE165554_RAW.tar GSM5039270_scSeq.rds.gz -C /seurat
+mv GSM5039270_G1_CellRanger_outs_barcodes.tsv.gz outs/g1
+mv GSM5039270_G1_CellRanger_outs_features.tsv.gz outs/g1
+mv GSM5039270_G1_CellRanger_outs_matrix.mtx.gz outs/g1
+
+mv GSM5039270_G2_CellRanger_outs_barcodes.tsv.gz outs/g2
+mv GSM5039270_G2_CellRanger_outs_features.tsv.gz outs/g2
+mv GSM5039270_G2_CellRanger_outs_matrix.mtx.gz outs/g2
+
+mv GSM5039270_scSeq.rds.gz seurat
